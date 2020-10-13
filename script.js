aa0c100161c0095aa4cde074bfdef1a7171aef66
Nader Frigui <nadfri@gmail.com>
2020-10-13 21:02:59 +0200

    ajout des coms dans le css

--- script.js
+++ script.js
@@ -67,18 +67,6 @@ function urgent(el)
 }
 
 
-/************Permettre le 100vh sur mobile */
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
-// // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-//   // We execute the same script as before
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
 
 //Empeche le resizing à l'apparition du clavier
 const metas = document.getElementsByTagName('meta');
