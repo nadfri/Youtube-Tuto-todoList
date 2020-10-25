77c663a66fb8c126a5175fe1247e54f5a0f85c54
Nader Frigui <nadfri@gmail.com>
2020-10-25 19:46:55 +0100

    test bouton install perso

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 "use strict";
 //verification si une clé existe dans le localStorage
-ol.innerHTML = (localStorage.getItem('list') != null)? 
-localStorage.getItem('list') : "";
+ol.innerHTML = (localStorage.getItem('list') != null) ?
+    localStorage.getItem('list') : "";
 
 //affiche l'info si aucune tache est en cours
-noTache.style.display = (ol.innerHTML == "")? "block" : "none";
+noTache.style.display = (ol.innerHTML == "") ? "block" : "none";
 
 //ajout de l'event del/urgent sur les span issus du storage
 const spanDels = document.querySelectorAll(".delete");
-for(let span of spanDels) span.onclick = () => del(span);
+for (let span of spanDels) span.onclick = () => del(span);
 
 const spanUrgs = document.querySelectorAll(".urgent");
-for(let span of spanUrgs) span.onclick = () => urgent(span);
+for (let span of spanUrgs) span.onclick = () => urgent(span);
 
 form.onsubmit = () => {
 
-    const li            = document.createElement("li");
-    const texte         = document.createElement("span");
+    const li = document.createElement("li");
+    const texte = document.createElement("span");
     texte.classList.add("texte");
-    texte.textContent   = champ.value; //recupere le texte du champ
-  
-    const spanDel       = document.createElement("span"); 
+    texte.textContent = champ.value; //recupere le texte du champ
+
+    const spanDel = document.createElement("span");
     spanDel.classList.add("material-icons", "delete", "md-24"); //ajout de la classe materiel-icons et delete
     spanDel.textContent = "delete_forever"; //ajout de l'icone poubelle
 
-    const spanUrg       = document.createElement("span");
+    const spanUrg = document.createElement("span");
     spanUrg.classList.add("material-icons", "urgent", "md-24"); //ajout de la classe materiel-icons et urgent
     spanUrg.textContent = "stars"; //ajout de l'icone stars
 
-    const spanOpt       = document.createElement("span");
+    const spanOpt = document.createElement("span");
     spanOpt.classList.add("spanOpt");
 
     spanOpt.appendChild(spanUrg);
@@ -39,7 +39,7 @@ form.onsubmit = () => {
 
     ol.appendChild(li); //ajout du li à ol
 
-    champ.value           = ""; // efface le champ
+    champ.value = ""; // efface le champ
     noTache.style.display = "none"; //enleve la div pas de tache en cours
 
     //ajout de l'event del et urgent sur les span créées
@@ -54,37 +54,63 @@ form.onsubmit = () => {
 };
 
 //fonction pour supprimer une tache
-function del(el){
+function del(el) {
     el.parentElement.parentElement.remove(); //supprime le parent et les enfants
     localStorage.setItem("list", ol.innerHTML); //met à jour le storage
-    noTache.style.display = (ol.innerHTML == "")? "block" : "none"; //met à jour la div tache en cours
+    noTache.style.display = (ol.innerHTML == "") ? "block" : "none"; //met à jour la div tache en cours
 }
 
-function urgent(el)
-{
+function urgent(el) {
     el.classList.toggle("gold");
     localStorage.setItem("list", ol.innerHTML); //save
 }
 
 //Empeche le resizing à l'apparition du clavier
 const metas = document.getElementsByTagName('meta');
-metas[1].content = 'width=device-width, height=' + window.innerHeight + ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
+metas[1].content = `width=device-width, height=${window.innerHeight} initial-scale=1.0, maximum-scale=5.0,user-scalable=0`;
 
-//*************Service Worker ******************/
+
 //Register service worker to control making site work offline
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('sw.js')
-        .then(registration => {
-          console.log(
-            `Service Worker enregistré!\nRessource: ${registration.scope}`
-          );
-        })
-        .catch(err => {
-          console.log(
-            `Echec de l'enregistrement du Service Worker: ${err}`
-          );
+        navigator.serviceWorker
+            .register('sw.js')
+            .then(registration => {
+                console.log(`Service Worker TodoList enregistré!\nRessource: ${registration.scope}`);
+            })
+            .catch(err => {
+                console.log(`Echec de l'enregistrement du Service Worker TodoList: ${err}`);
+            });
+    });
+}
+
+
+/**Bouton Installer */
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'flex';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
         });
     });
-  }
+  });
--- sw.js
+++ sw.js
@@ -1,32 +1,10 @@
-console.log("Script /sw.js Chargé!");
-
-// const CACHE  = "todo-cache";
-// const urlsToCache = [
-//     '**.html'
-
-// ];
-
-
-// self.addEventListener('install', event => {
-//     console.log("Service Worker installé!");
-//     event.waitUntil(
-//         caches.open(CACHE)
-//         .then(cache => {
-//         return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-
-// self.addEventListener('activate', event => {
-//     console.log("Service Worker activé!");
-// });
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+
 if (workbox) {
-  console.log(`WorkBox of  loaded🎉`);
+  console.log(`WorkBox of TodoList loaded🎉`);
   workbox.routing.registerRoute(
-    new RegExp('/*/.*'), //cached all files
+    new RegExp('/.*'), //cached all files
     new workbox.strategies.StaleWhileRevalidate()
   );
 
@@ -38,7 +16,7 @@ if (workbox) {
     })
   );
   
-  // Cache the underlying font files with a cache-first strategy for 1 year.
+  // Cache the underlying font files with a cache-first
   workbox.routing.registerRoute(
     ({url}) => url.origin === 'https://fonts.gstatic.com',
     new workbox.strategies.CacheFirst({
@@ -47,4 +25,4 @@ if (workbox) {
     })
   );
 
-} else {console.log(`Boo! Workbox didn't load 😬`);}
+} else {console.log(`Boo! Workbox of TodoList didn't load 😬`);}
