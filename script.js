8f696e417d799bf0869da11d60fd84a946ca5c85
Nader Frigui <nadfri@gmail.com>
2020-10-25 20:24:28 +0100

    bouton install avec animation css

--- script.js
+++ script.js
@@ -87,8 +87,6 @@ if ('serviceWorker' in navigator) {
 
 /**Bouton Installer */
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -96,11 +94,10 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'flex';
-  
     addBtn.addEventListener('click', (e) => {
       // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
+      addBtn.classList.add("down");
+      console.log("ok")
       // Show the prompt
       deferredPrompt.prompt();
       // Wait for the user to respond to the prompt
@@ -114,3 +111,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
         });
     });
   });
+
+
+
