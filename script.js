20967eaa811bd183649d4d84330735a6815a55ab
Nader Frigui <nadfri@gmail.com>
2020-10-26 18:48:29 +0100

    ajout transition left aulieu de down

--- script.js
+++ script.js
@@ -84,55 +84,23 @@ if ('serviceWorker' in navigator) {
 }
 
 /**Bouton Installation Application*/
-// let deferredPrompt;
-
-// window.addEventListener('beforeinstallprompt', (e) => 
-// {
-//     e.preventDefault(); // annuler la banniere par defaut
-//     installBtn.classList.add("slide"); //affiche la banniere perso
-//     deferredPrompt = e; //enregistrer l'event pour plus tard
-
-//     installBtn.addEventListener('click', (e) => 
-//     {
-//         deferredPrompt.prompt(); //permettre l'installation
-//         installBtn.classList.remove("slide"); //faire disparaitre le bouton
-
-//         deferredPrompt.userChoice.then((choiceResult) => {
-//             if (choiceResult.outcome === 'accepted') 
-//                 installBtn.classList.remove("slide"); //faire disparaitre le bouton
-
-//             deferredPrompt = null; //efface l'event
-//       });
-//     });
-// });
-
 let deferredPrompt;
-installBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    installBtn.style.display = 'block';
-  
-    installBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      installBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
-  });
+window.onbeforeinstallprompt = (e) => 
+{
+    e.preventDefault(); // annuler la banniere par defaut
+    installBtn.classList.add("slide"); //affiche la banniere perso
+    deferredPrompt = e; //enregistrer l'event pour plus tard
+
+    installBtn.onclick = () => 
+    {
+        installBtn.classList.remove("slide"); //faire disparaitre le bouton
+        deferredPrompt.prompt(); //permettre l'installation
+        deferredPrompt = null; //efface l'event
+    };
+};
+
+
 
 
 
