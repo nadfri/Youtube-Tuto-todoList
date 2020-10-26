a2742127bd51167a658ee2c9978cb5e7ac5d7805
Nader Frigui <nadfri@gmail.com>
2020-10-26 11:56:03 +0100

    simplification code bouton install

--- script.js
+++ script.js
@@ -89,25 +89,20 @@ if ('serviceWorker' in navigator) {
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    addBtn.classList.remove("down");
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      addBtn.classList.add("down");
-      console.log("ok")
-      // Show the prompt
-      deferredPrompt.prompt();
+    e.preventDefault(); // annuler la banniere par defaut
+    installBtn.classList.remove("down"); //faire apparaitre le bouton
+    deferredPrompt = e; //enregistrer l'event pour plutard
+
+    installBtn.addEventListener('click', (e) => {
+      installBtn.classList.add("down"); //faire disparaitre le bouton
+      deferredPrompt.prompt(); //permettre l'installation
       // Wait for the user to respond to the prompt
       deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
+        //   if (choiceResult.outcome === 'accepted') {
+        //     console.log('User accepted the A2HS prompt');
+        //   } else {
+        //     console.log('User dismissed the A2HS prompt');
+        //   }
           deferredPrompt = null;
         });
     });
