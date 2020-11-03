c108898e4dad33705f0b9d775c7fa3ad603f72e1
Nader Frigui <nadfri@gmail.com>
2020-11-03 21:02:32 +0100

    ajout cross origin sur google font

--- sw.js
+++ sw.js
@@ -1,27 +1,34 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+console.log(`WorkBox of TodoList loaded🎉`);
+
 const {registerRoute}        = workbox.routing;
 const {StaleWhileRevalidate} = workbox.strategies;
 const {CacheFirst}           = workbox.strategies;
+const {ExpirationPlugin}     = workbox.expiration;
 
-if (workbox) 
-{
-  console.log(`WorkBox of TodoList loaded🎉`);
+registerRoute(
+  ({url}) => url.origin === 'https://fonts.googleapis.com' ||
+              url.origin === 'https://fonts.gstatic.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({maxEntries: 20}),
+    ],
+  }),
+);
 
-  registerRoute(
-    ({request}) => request.destination === 'document'||
-                   request.destination === 'script'  ||
-                   request.destination === 'style',
-                   
-    new StaleWhileRevalidate()
-  );
+registerRoute(
+  ({request}) => request.destination === 'document'||
+                 request.destination === 'script'  ||
+                 request.destination === 'style',      
+  new StaleWhileRevalidate({cacheName: 'files-cache'})
+);
 
-  registerRoute(
-    // Cache image files.
-    ({request}) => request.destination === 'image',
-    // Use the cache if it's available.
-    new CacheFirst({cacheName: 'image-cache',}) // Use a custom cache name.
-  );
-}
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst({cacheName: 'image-cache'}) // Use a custom cache name.
+);
 
-else { console.log(`Boo! Workbox of TodoList didn't load 😬`); }
+
+
