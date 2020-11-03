73dcd3aa014231fa596076678431a01bad3708d9
Nader Frigui <nadfri@gmail.com>
2020-11-03 16:03:06 +0100

    simplification du sw

--- sw.js
+++ sw.js
@@ -1,40 +1,23 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+const {registerRoute}        = workbox.routing;
+const {StaleWhileRevalidate} = workbox.strategies;
+const {CacheFirst}           = workbox.strategies;
 
-if (workbox) {
+if (workbox) 
+{
   console.log(`WorkBox of TodoList loaded🎉`);
-  workbox.routing.registerRoute(
-    new RegExp('/.*'), //cached all files
-    new workbox.strategies.StaleWhileRevalidate()
+
+  registerRoute(
+    ({request}) => request.destination === 'document'||
+                   request.destination === 'script'  ||
+                   request.destination === 'style',               
+    new StaleWhileRevalidate()
   );
 
-  workbox.routing.registerRoute(
-    ({ url }) => url.origin === 'https://fonts.googleapis.com' ||
-      url.origin === 'https://fonts.gstatic.com',
-      new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'google-fonts',
-      plugins: [
-        new workbox.expiration.ExpirationPlugin({ maxEntries: 20 }),
-      ],
-    }),
-  );
+  ({request}) => request.destination === 'image',
+    // Use the cache if it's available.
+    new CacheFirst({cacheName: 'image-cache',});
+}
 
-
-  // // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-  // workbox.routing.registerRoute(
-  //   ({ url }) => url.origin === 'https://fonts.googleapis.com',
-  //   new workbox.strategies.StaleWhileRevalidate({
-  //     cacheName: 'google-fonts-stylesheets',
-  //   })
-  // );
-
-  // // Cache the underlying font files with a cache-first
-  // workbox.routing.registerRoute(
-  //   ({ url }) => url.origin === 'https://fonts.gstatic.com',
-  //   new workbox.strategies.CacheFirst({
-  //     cacheName: 'google-fonts-webfonts',
-
-  //   })
-  // );
-
-} else { console.log(`Boo! Workbox of TodoList didn't load 😬`); }
+else { console.log(`Boo! Workbox of TodoList didn't load 😬`); }
